b5c1850c352db63db2d0d057aeefb6350b77e29e
Pedro Oliveira <alvesp2001@gmail.com>
2022-07-03 14:36:18 -0300

    refactor(Error Handler): changing "err" to "error"

--- src/infra/http/express/middlawares/error-handler.ts
+++ src/infra/http/express/middlawares/error-handler.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 import { BaseError } from "@infra/http/errors/base-error";
 
-export function ErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    if (err instanceof BaseError) {
-        return res.status(err.statusCode).send({ error: err.message });
+export function ErrorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
+    if (error instanceof BaseError) {
+        return res.status(error.statusCode).send({ error: error.message });
     }
     return res.status(500).send({ error: "Internal Server Error" });
 }
