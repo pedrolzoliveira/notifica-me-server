ebffc386e746ff04d88854ec9c299b1d5c05fb67
Pedro Oliveira <pluiz@mychirotouch.com>
2022-10-02 23:30:27 -0300

    return plans with events

--- src/application/services/plans.service.ts
+++ src/application/services/plans.service.ts
@@ -1,7 +1,7 @@
 import { CreatePlan } from '@application/dtos/create-plan.dto'
 import { UpdatePlan } from '@application/dtos/update-plan.dto'
 import { PlansRepository } from '@application/repositories/plans/plans.repository'
-import { Plan, PrismaClient } from '@prisma/client'
+import { EventType, Plan, PrismaClient } from '@prisma/client'
 
 export class PlansService {
   constructor(
@@ -29,12 +29,50 @@ export class PlansService {
     return await this.plansRepository.delete(id)
   }
 
-  findByQuery(q: string) {
+  async findByQuery(q: string) {
+    type queryResult = Plan & { EventType: EventType }
     const likeSearch = `%${q}%`
-    return this.db.$queryRaw<Plan[]>`SELECT *
+    const result = await this.db.$queryRaw<queryResult[]>`
+    SELECT "Plan".*,
+    "EventType"."code" "EventType.code",
+    "EventType"."name" "EventType.name",
+    "EventType"."description" "EventType.description",
+    "EventType"."createdAt" "EventType.createdAt",
+    "EventType"."updatedAt" "EventType.updatedAt"
     FROM "Plan"
-    WHERE UPPER("name") LIKE UPPER(${likeSearch})
-    OR UPPER("description") LIKE UPPER(${likeSearch})`
+    JOIN "_EventTypeToPlan"
+    ON ("_EventTypeToPlan"."B" = "Plan"."id")
+    JOIN "EventType"
+    ON ("_EventTypeToPlan"."A" = "EventType"."code")    
+    WHERE UPPER("Plan"."name") LIKE UPPER(${likeSearch})
+    OR UPPER("Plan"."description") LIKE UPPER(${likeSearch})`
+    return result.reduce((formatedPlans, currentPlan, index, arr) => {
+      if (formatedPlans.some(v => v.id === currentPlan.id)) return formatedPlans
+      const formatedcurrentPlan = {
+        id: currentPlan.id,
+        name: currentPlan.name,
+        description: currentPlan.description,
+        price: currentPlan.price,
+        events: arr.reduce((formatedEvents, currentEvent) => {
+          if (currentEvent.id === currentPlan.id) {
+            const formatedEvent = {
+              code: currentEvent['EventType.code'],
+              name: currentEvent['EventType.name'],
+              adminId: currentPlan.adminId,
+              description: currentEvent['EventType.description'],
+              createdAt: currentEvent['EventType.createdAt'],
+              updatedAt: currentEvent['EventType.updatedAt']
+            }
+            formatedEvents.push(formatedEvent)
+          }
+          return formatedEvents
+        }, []),
+        createdAt: currentPlan.createdAt,
+        updatedAt: currentPlan.updatedAt
+      }
+      formatedPlans.push(formatedcurrentPlan)
+      return formatedPlans
+    }, [])
   }
 
   async findAllByCustomer(customerId: string) {
